c7775b5186f56aaf209296a1a0059fc153acae9f
Gman349 <obri9773@student.acitech.org>
2024-05-16 09:18:33 -0400

    PONG innitial commit

---
+++ script.js
@@ -0,0 +1,125 @@
+//get elements
+
+let canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+//static variables
+
+const width = canvas.width;
+const height = canvas.height;
+
+// build play area
+ctx.fillStyle = "black";
+ctx.fillRect(0,0,width,height);
+
+// ~~~BALL~~~
+
+// dictate the size of the ball
+const ball_size = 10;
+// dictate starting position
+var ball_move = { x:20 , y:20 };
+
+// horizontal and vertical rate of change
+var x_speed = 3;
+var y_speed = 1;
+
+//ball collide
+
+function ball_collide() {
+    //creates local object that indicates the location of the sides of the ball
+    var ball = { 
+        left: ball_move.x,
+        right: ball_move.x + ball_size,
+        top: ball_move.y,
+        bottom: ball_move.y + ball_size
+    }
+
+    var left_paddle = { //indicates location of left paddle
+        left:paddle_offset,
+        right:paddle_offset + paddle_width,
+        top: left_paddle_top,
+        bottom: left_paddle_top + paddle_height
+    }
+
+    
+    var right_paddle = {  //indicates location of right paddle
+        left:width - paddle_offset - paddle_width,
+        right:width - paddle_offset,
+        top:right_paddle_top,
+        bottom:right_paddle_top + paddle_height
+    }
+
+    //if ball collides with left/right edge of play area
+    if (ball.left < 0 || ball.right > width) {
+        x_speed = -x_speed;//reverse direction
+    }
+    //if ball collides with top/bottom edge of play area
+    if (ball.top <0 || ball.bottom > height) {
+        y_speed = -y_speed;//reverse direction
+    }
+
+    paddle_collide(ball,left_paddle);
+    paddle_collide(ball,right_paddle);
+}
+
+
+function paddle_collide(chk_ball,chk_paddle) {
+
+    if (chk_ball.left < chk_paddle.right && chk_ball.right> chk_paddle.left && chk_ball.top < chk_paddle.bottom && chk_ball.bottom > chk_paddle.top) {
+        x_speed = -x_speed
+    } 
+
+}
+
+
+// ~~~PADDLE~~~
+//size of paddles
+const paddle_width =5;
+const paddle_height=30;
+const paddle_offset = 10;
+// paddle offset from top of canvas
+var left_paddle_top = 30;
+var right_paddle_top = 30;
+
+//draws the current state of canvas
+function draw() {
+
+    // build play area
+    ctx.fillStyle = "black";
+    ctx.fillRect(0,0,width,height);
+    //draw/redraw ball
+    ctx.fillStyle = "white";
+    ctx.fillRect(ball_move.x,ball_move.y,ball_size,ball_size);
+    //draw/redraw paddles
+    ctx.fillRect(paddle_offset,left_paddle_top,paddle_width,paddle_height);//left
+    ctx.fillRect((width - paddle_offset - paddle_width),right_paddle_top,paddle_width,paddle_height);//right
+}
+//update the ball's current xy coordinate
+function update_ball() {
+    ball_move.x += x_speed;
+    ball_move.y += y_speed;
+}
+
+//main game loop
+function game_loop() {
+
+    update_ball();
+    draw();
+    ball_collide();
+    setTimeout(game_loop, 30);
+}
+
+
+//right paddle movement
+document.addEventListener("mousemove",(event) => {
+
+    right_paddle_top = event.offsetY;
+
+})
+
+
+
+// call main loop to start the game
+
+game_loop();
+
